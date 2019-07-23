ea9a52aee3bd2eb8a2ac56d45a5064d760b84ca8
CHAMATH <CHAMATH@DESKTOP-B4L7R4F>
2019-07-23 11:50:02 -0700

    this is test application 3

--- src/test1/Bufferwithoutpool.java
+++ src/test1/Bufferwithoutpool.java
@@ -71,7 +71,7 @@ public class Bufferwithoutpool extends Thread {
         }
         System.out.println("get momory total    " + rn.totalMemory());
         System.out.println("g after    " + rn.freeMemory());
-        System.out.print("used mmannnnnnn       ");
+        System.out.print("used kkkknnnnnn       ");
         System.out.println(rn.totalMemory() - rn.freeMemory());
         System.out.println("");
 
