51aeb8013fdb7653fdbf61f07e53d3f1e6e78a98
CHAMATH <CHAMATH@DESKTOP-B4L7R4F>
2019-07-25 10:04:35 -0700

    chamath commit 3

---
+++ src/test1/Bufferoneobject.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Bufferoneobject {
+
+    HashMap<String, Integer> wordCountMap = new HashMap<String, Integer>();
+    public final Runnable typeA;
+    public final Runnable typeB;
+    Runtime rn = Runtime.getRuntime();
+    static long startTime;
+    static long endTime;
+    static Bufferoneobject x = new Bufferoneobject();
+
+    public Bufferoneobject() {
+        typeA = new Runnable() {
+            public void run() {
+                BufferedReader reader = null;
+                try {
+
+                    reader = new BufferedReader(new FileReader("C:\\Users\\CHAMATH\\Desktop\\test.txt"),16384);
+
+                    String currentLine = reader.readLine();
+                    while (currentLine != null) {
+
+                        String[] wordsbunch = currentLine.trim().toLowerCase().split(" ",0);
+                        for (String word : wordsbunch) {
+
+                            if (wordCountMap.containsKey(word)) {
+                                wordCountMap.put(word, wordCountMap.get(word) + 1);
+                            } else {
+                                wordCountMap.put(word, 1);
+                            }
+                        }
+
+                        currentLine = reader.readLine();
+
+                        // get information of type A
+                        // return the data or directly store in OnlineResourceAdapter.
+                    }
+
+                    x.typeB.run();
+                } catch (IOException e) {
+                } finally {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        typeB = new Runnable() {
+            public void run() {
+
+                Set<Map.Entry<String, Integer>> entrySet = wordCountMap.entrySet();
+
+                for (Map.Entry<String, Integer> entry : entrySet) {
+                    System.out.print(entry.getKey() + ": ");
+                    System.out.println(entry.getValue());
+                }
+                endTime = System.nanoTime();
+                long totalTime = (endTime - startTime) / 1000000;
+                System.out.println("Runtime    MS:");
+                System.out.println(totalTime);
+
+            }
+        };
+
+    }
+
+    public static void main(String args[]) {
+        startTime = System.nanoTime();
+        x.typeA.run();// start A
+        // start B
+
+// <1>can be accessed here.
+    }
+
+}
